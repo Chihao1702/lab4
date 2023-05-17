1b65cb5f4124be05ef28728417a022e723978335
Chihao1702 <117892988+Chihao1702@users.noreply.github.com>
2023-05-17 15:31:02 +0700

    Add files via upload

---
+++ lab4bai2/Main.java
@@ -0,0 +1,11 @@
+
+package lab4bai2;
+public class Main {
+
+    public static void main(String[] args) {
+        QuanLyKhachHang ql=new QuanLyKhachHang();
+        ql.NhapThongTin();
+        ql.XuatThongTin();
+    }
+    
+}
