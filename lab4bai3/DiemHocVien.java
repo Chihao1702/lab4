a03e1f9eda842cf0e14d37ee6b0c627893be9e2c
Chihao1702 <117892988+Chihao1702@users.noreply.github.com>
2023-05-17 15:38:28 +0700

    Add files via upload

---
+++ lab4bai3/DiemHocVien.java
@@ -0,0 +1,29 @@
+
+package lab4bai3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class DiemHocVien {
+    private String hoTen;
+    private int namSinh;
+    private List<Double> diemMonHoc;
+
+    public DiemHocVien(String hoTen, int namSinh, List<Double> diemMonHoc) {
+        this.hoTen = hoTen;
+        this.namSinh = namSinh;
+        this.diemMonHoc = diemMonHoc;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public int getNamSinh() {
+        return namSinh;
+    }
+
+    public List<Double> getDiemMonHoc() {
+        return diemMonHoc;
+    }
+}
